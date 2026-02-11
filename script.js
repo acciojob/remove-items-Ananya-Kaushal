c77f48efbd00dbf2e864d8f37969aab3494f7398
Ananya Kaushal <62956565+Ananya-Kaushal@users.noreply.github.com>
2026-02-11 23:56:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function removeSelectedColor() {
+    // 1. Get the dropdown element by its ID
+    const dropdown = document.getElementById("colorSelect");
+
+    // 2. Get the index of the currently selected option
+    const selectedIndex = dropdown.selectedIndex;
+
+    // 3. Remove the option if something is actually selected
+    if (selectedIndex !== -1) {
+        dropdown.remove(selectedIndex);
+    } else {
+        alert("Please select a color to remove.");
+    }
+}
